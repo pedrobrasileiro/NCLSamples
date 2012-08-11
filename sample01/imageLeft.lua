4ee612ce2765d65c66e59f35a0824ee8cea44856
root <root@gingavm.localdomain>
2012-08-10 23:23:01 -0300

    Added focus

--- sample01/imageLeft.lua
+++ sample01/imageLeft.lua
@@ -1,5 +1,3 @@
-   canvas:attrColor(252,2,108,0)
-   -- canvas:clear()
-   canvas:attrFont("vera", 24)
-   canvas:drawText(30, 400, "Ola mundo")
-   canvas:flush() 
+local img = canvas:new("image1.jpg")
+canvas:compose(0,0, img )
+        canvas:flush()
