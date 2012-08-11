b9899af288d9aeed22cd77334d9b5ea74cc853bd
root <root@gingavm.localdomain>
2012-08-10 23:40:51 -0300

    Events

--- sample01/imageLeft.lua
+++ sample01/imageLeft.lua
@@ -1,3 +1,8 @@
 local img = canvas:new("image1.jpg")
 canvas:compose(0,0, img )
         canvas:flush()
+
+function handler (evt)
+  print(evt.class)
+end
+event.register(handler) 
--- sample01/imageRight.lua
+++ sample01/imageRight.lua
@@ -1,3 +1,9 @@
 local img = canvas:new("cartoon.png")
 canvas:compose(0,0, img )
         canvas:flush()
+function printText()
+
+  print("Ola!")
+  event.post ('in', { class='user', data='mydata' })
+end
+event.timer(1000,printText)
